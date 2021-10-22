671ccf262f4fcfce0e3f9156d592c2601a349fed
dkukadiya <dkukadiya123@gmail.com>
2021-10-22 20:16:51 +0100

    i changed to numbers

--- src/Test4.java
+++ src/Test4.java
@@ -1,8 +1,8 @@
 public class Test4 {
     public static void main(String[] args){
-        System.out.println("I like git");
-        System.out.println("I like git");
-        System.out.println("I like git");
-        System.out.println("I like git");
+        System.out.println("I like git1");
+        System.out.println("I like git2");
+        System.out.println("I like git3");
+        System.out.println("I like git4");
     }
 }
